2da61c61e6a9d4a957855a965745288901c9ce12
liyuKnow <kidus2011@gmail.com>
2023-03-04 05:40:27 -0800

    final commit

--- admin/src/views/admin/Clients/index.jsx
+++ admin/src/views/admin/Clients/index.jsx
@@ -24,7 +24,7 @@ const Clients = () => {
       email: "jean@gmail.com",
       age: 44,
       phone: "+2519-11-222324",
-      access: "admin",
+      access: "user",
     },
   ];
 
@@ -88,7 +88,34 @@ const Clients = () => {
   return (
     <Box m="20px">
       {/* <Header title="TEAM" subtitle="Managing the Team Members" /> */}
-      <Box height="75vh">
+      <Box
+        height="75vh"
+        // sx={{
+        //   "& .MuiDataGrid-root": {
+        //     border: "none",
+        //   },
+        //   "& .MuiDataGrid-cell": {
+        //     borderBottom: "none",
+        //   },
+        //   "& .name-column--cell": {
+        //     color: colors.greenAccent[300],
+        //   },
+        //   "& .MuiDataGrid-columnHeaders": {
+        //     backgroundColor: colors.blueAccent[700],
+        //     borderBottom: "none",
+        //   },
+        //   "& .MuiDataGrid-virtualScroller": {
+        //     backgroundColor: colors.primary[400],
+        //   },
+        //   "& .MuiDataGrid-footerContainer": {
+        //     borderTop: "none",
+        //     backgroundColor: colors.blueAccent[700],
+        //   },
+        //   "& .MuiCheckbox-root": {
+        //     color: `${colors.greenAccent[200]} !important`,
+        //   },
+        // }}
+      >
         <DataGrid checkboxSelection rows={data} columns={columns} />
       </Box>
     </Box>
