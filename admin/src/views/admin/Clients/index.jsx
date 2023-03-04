7b312fc180b427d1f074a7bc34788aefa1c54d42
liyuKnow <kidus2011@gmail.com>
2023-03-04 05:03:31 -0800

    data grid

--- admin/src/views/admin/Clients/index.jsx
+++ admin/src/views/admin/Clients/index.jsx
@@ -1,7 +1,98 @@
-import React from "react";
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../../context/themeContext";
+
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
 
 const Clients = () => {
-  return <div>Clients</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const data = [
+    {
+      id: 1,
+      name: "John Doe",
+      email: "john@gmail.com",
+      age: 44,
+      phone: "+2519-11-121314",
+      access: "admin",
+    },
+    {
+      id: 2,
+      name: "Jean Doe",
+      email: "jean@gmail.com",
+      age: 44,
+      phone: "+2519-11-222324",
+      access: "admin",
+    },
+  ];
+
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "accessLevel",
+      headerName: "Access Level",
+      flex: 1,
+      renderCell: ({ row: { access } }) => {
+        return (
+          <Box
+            width="60%"
+            m="0 auto"
+            p="5px"
+            display="flex"
+            justifyContent="center"
+            backgroundColor={
+              access === "admin"
+                ? colors.greenAccent[600]
+                : access === "manager"
+                ? colors.greenAccent[700]
+                : colors.greenAccent[700]
+            }
+            borderRadius="4px"
+          >
+            {access === "admin" && <AdminPanelSettingsOutlinedIcon />}
+            {access === "manager" && <SecurityOutlinedIcon />}
+            {access === "user" && <LockOpenOutlinedIcon />}
+            <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
+              {access}
+            </Typography>
+          </Box>
+        );
+      },
+    },
+  ];
+  return (
+    <Box m="20px">
+      {/* <Header title="TEAM" subtitle="Managing the Team Members" /> */}
+      <Box height="75vh">
+        <DataGrid checkboxSelection rows={data} columns={columns} />
+      </Box>
+    </Box>
+  );
 };
 
 export default Clients;
